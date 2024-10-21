439980ccb32ecb86988eaf278f0964a381f67e83
Himanshu Sharma <contact@himanshusharma.tech>
2024-10-21 19:03:51 +0530

    ci fix

--- src/middlewares/errorhandler.js
+++ src/middlewares/errorhandler.js
@@ -1,5 +1,6 @@
 // errorhandler.js - Middleware to handle errors
-module.exports = (err, req, res) => {
+// eslint-disable-next-line no-unused-vars
+module.exports = (err, req, res, next) => {
   console.error(err.stack); // Log the error stack
   res.status(500).json({ error: err.message }); // Send JSON error response
 };
